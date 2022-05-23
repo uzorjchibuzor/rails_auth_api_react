1f6de86c91a8a36f51333fcf2527dfaecac3f973
Uzor Efedigbue <cefedigbuej@gmail.com>
2022-05-23 11:16:49 +0100

    configure Rails API Session store to work in all environments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domian: 'https://auth-app-api-rails-react.herokuapp.com/'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://auth-app-api-rails-react.herokuapp.com/'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
