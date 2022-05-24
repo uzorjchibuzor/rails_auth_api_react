e23030d868e77351bdf9cb8d594e7a2e2728287a
Uzor Efedigbue <cefedigbuej@gmail.com>
2022-05-24 16:29:10 +0100

    edit allow cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://auth-app-api-rails-react.herokuapp.com/'
+    origins 'https://uzor-react-auth.herokuapp.com/'
     resource '*', headers: :any, methods: %i[get post patch put delete options head], credentials: true
   end
 end
